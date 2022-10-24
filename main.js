986897548dfa520d17d23533e986c0f81b98edaa
v.a.martynenko <v.a.martynenko@macbook-v.a.martynenko>
2022-10-24 18:31:57 +0300

    done 3 task

--- main.js
+++ main.js
@@ -2,11 +2,23 @@
 
 
 document.getElementById("area").onblur = function() {
-   let words = document.getElementById("area").value.split(' ').length;
-   let symbols = document.getElementById("area").value.length;
+   let data = document.getElementById("area").value;
+   let words = data.split(' ').length;
+   let symbols = data.length;
+   let symbols_without_spaces  = data.length - count_spaces(data);
+   console.log(count_spaces(data));
    document.getElementById("value_words").innerText = words;
    document.getElementById("value_symbols").innerText = symbols; 
+   document.getElementById("value_symbols_without").innerText = symbols_without_spaces; 
 
+}
+
+function count_spaces(data) {
+   let counter = 0;
+   for (let i=0; i<data.length;i++) {
+      if (data[i]==" ") counter=counter+1;
+   }
+   return counter
 }
 
 
