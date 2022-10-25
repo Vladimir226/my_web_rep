bafe2dbec431b26a6abd703322fef0141364fcc3
Vladimir226 <vovanbotan22@gmail.com>
2022-10-25 21:35:43 +0300

    create funct: create_board and filling_cells

--- main.js
+++ main.js
@@ -1,80 +1,54 @@
 
 
+let random_bombs = [0,1];
 
-document.getElementById("area").onblur = function() {
-   analyzer();
-}
+create_board(2);
+filling_cells(2);
 
+function filling_cells(size) {
 
+   let cells = new Array(size);
 
-function analyzer() {
-   let data = document.getElementById("area").value;
-
-
-
-   if (document.getElementById("check_value_words").checked)
-   {
-      if(data!="") {
-         let words = data.split(/[\n,\s]/);
-         console.log(words);
-         words = words.filter(function(f){return f!=""});
-         console.log(words);
-         document.getElementById("value_words").innerText = words.length;
+   for(let i=0;i<size;i++)
+   {  
+      cells[i] = [];
+      for(let j=0;j<size;j++)
+      {
+         let cell =
+            {
+               id: "",
+               open: false,
+               bomb: false,
+            }
+         console.log(`test_${i}_${j}`);
+         cell.id = `cell_${i}_${j}`;
+         if (random_bombs.includes(i*size+j)) cell.bomb=true;
+         
+         cells[i].push(cell);
       }
    }
-   else document.getElementById("value_words").innerText = ""
-
-   if (document.getElementById("check_value_symbols").checked)
-   {
-      let symbols = data.length;
-      document.getElementById("value_symbols").innerText = symbols;
-   }
-   else document.getElementById("value_symbols").innerText = "";
-
-   if (document.getElementById("check_value_symbols_without").checked)
-   {
-      let symbols_without_spaces  = data.length - count_spaces(data);
-      document.getElementById("value_symbols_without").innerText = symbols_without_spaces; 
-   }
-   else document.getElementById("value_symbols_without").innerText = ""; 
-
-   if (document.getElementById("check_value_alphabet").checked)
-   {
-      console.log(count_alphabet(data));
-   }
-   else  document.getElementById("alphabet").innerHTML = "";
+   console.log(cells);
 }
 
-function count_spaces(data) {
-   let counter = 0;
-   for (let i=0; i<data.length;i++) {
-      if (data[i]==" ") counter=counter+1;
-   }
-   return counter
-}
-
-function count_alphabet(data) {
-
-   document.getElementById("alphabet").innerHTML = ""
-
-   let counter_alphabet = new Object();
-
-   for (let i =0; i<data.length;i++) {
-      if(isNaN(counter_alphabet[data[i]])) counter_alphabet[data[i]] = 0
-      counter_alphabet[data[i]] = counter_alphabet[data[i]]+1;
-   }
-
-
-   for (let key in counter_alphabet)
+function create_board(size)
+{
+   let board = document.createElement('table');
+   board.className="game_board";
+   for(let i=0;i<size;i++)
    {
-      let div = document.createElement('div');
-      if (key==='\n') div.innerText=`"\\n" - ${counter_alphabet[key]}`;
-      else div.innerText=`"${key}" - ${counter_alphabet[key]}`;
-      document.getElementById("alphabet").appendChild(div);
+      let row = document.createElement('tr');
+      board.appendChild(row);
+      for(let j=0;j<size;j++)
+      {
+         let column = document.createElement('td');
+         row.appendChild(column);
+
+         let button = document.createElement('button');
+         button.className="cell"
+         button.id = `cell_${i}_${j}`;
+
+         column.appendChild(button);
+      }
    }
-
-   return counter_alphabet;
+   document.getElementById("board_container").appendChild(board);
 }
-
-
-
