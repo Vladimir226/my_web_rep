7569e06d77886fcb4dcb45e22a0637d279e2cd8f
v.a.martynenko <v.a.martynenko@macbook-v.a.martynenko>
2022-10-25 11:49:59 +0300

    done 4 task

--- main.js
+++ main.js
@@ -2,17 +2,42 @@
 
 
 document.getElementById("area").onblur = function() {
+   analyzer();
+}
+
+
+
+function analyzer() {
    let data = document.getElementById("area").value;
-   console.log(count_alphabet(data));
 
-   let words = data.split(' ').length;
-   let symbols = data.length;
-   let symbols_without_spaces  = data.length - count_spaces(data);
-   console.log(count_spaces(data));
-   document.getElementById("value_words").innerText = words;
-   document.getElementById("value_symbols").innerText = symbols; 
-   document.getElementById("value_symbols_without").innerText = symbols_without_spaces; 
+   console.log(document.getElementById("check_value_words").checked);
 
+   if (document.getElementById("check_value_words").checked)
+   {
+      let words = data.split(' ').length;
+      document.getElementById("value_words").innerText = words;
+   }
+   else document.getElementById("value_words").innerText = ""
+
+   if (document.getElementById("check_value_symbols").checked)
+   {
+      let symbols = data.length;
+      document.getElementById("value_symbols").innerText = symbols;
+   }
+   else document.getElementById("value_symbols").innerText = "";
+
+   if (document.getElementById("check_value_symbols_without").checked)
+   {
+      let symbols_without_spaces  = data.length - count_spaces(data);
+      document.getElementById("value_symbols_without").innerText = symbols_without_spaces; 
+   }
+   else document.getElementById("value_symbols_without").innerText = ""; 
+
+   if (document.getElementById("check_value_alphabet").checked)
+   {
+      console.log(count_alphabet(data));
+   }
+   else  document.getElementById("alphabet").innerHTML = "";
 }
 
 function count_spaces(data) {
@@ -31,7 +56,7 @@ function count_alphabet(data) {
 
    for (let i =0; i<data.length;i++) {
       if(isNaN(counter_alphabet[data[i]])) counter_alphabet[data[i]] = 1
-      counter_alphabet[data[i]] = counter_alphabet[data[i]]+1;
+      else counter_alphabet[data[i]] = counter_alphabet[data[i]]+1;
    }
 
 
