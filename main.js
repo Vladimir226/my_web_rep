638c84b1973fb571c1f75a4562da8d3611ab337f
Vladimir226 <vovanbotan22@gmail.com>
2022-10-26 21:51:35 +0300

    make settings window make statistics

--- main.js
+++ main.js
@@ -9,14 +9,19 @@ let value_cur_time=0;
 
 const click_color = "blue";
 const bomb_color = "red";
-const size=5;
-const bombs=10;
-const game_time = 20
 
 
 
 function Game_start()
 {
+   window.size = parseInt(document.getElementById("input_size").value);
+   window.bombs = parseInt(document.getElementById("input_bombs").value);
+   window.game_time = parseInt(document.getElementById("input_time").value);
+   Show_statistics();
+   document.getElementById('title_timer').innerText = `Осталось ${game_time-value_cur_time} секунд`;
+   document.getElementById('title_bombs').innerText = `Осталось ${bombs} бомб`;
+   document.getElementById('settings').style.display = "none";
+   document.getElementById('game').style.display = "flex";
    timer_start();
    create_random_boms(size,bombs);
    filling_cells(size);
@@ -25,9 +30,17 @@ function Game_start()
 
 function Game_restart()
 {
+   timer_stop();
    Board_clear();
    Data_clear();
    Game_start();
+}
+
+function Game_stop()
+{
+   timer_stop();
+   Board_clear();
+   Data_clear();
 }
 
 function Board_clear()
@@ -71,6 +84,12 @@ function create_board(size)
 {
    let board = document.createElement('table');
    board.className="game_board";
+   
+   document.getElementById("game").style.width =document.documentElement.clientWidth;
+   //document.getElementById("game").style.h =document.documentElement.clientWidth;
+
+   board.style.width = document.documentElement.clientWidth;
+   board.style.height = document.documentElement.clientWidth;
    for(let i=0;i<size;i++)
    {
       let row = document.createElement('tr');
@@ -107,6 +126,7 @@ function create_board(size)
                         if(!clicked_bombs.includes(i*size+j))
                         {
                            clicked_bombs.push(i*size+j);
+                           document.getElementById('title_bombs').innerText = `Осталось ${random_bombs.length-clicked_bombs.length} бомб`
                         }
                      }
                   }
@@ -156,6 +176,7 @@ function timer_stop() {
 //Эта функция меняет value для инпута
 function timer() {
 	value_cur_time=value_cur_time+1
+   document.getElementById('title_timer').innerText = `Осталось ${game_time-value_cur_time} секунд`
    console.log(value_cur_time);
    if (value_cur_time==game_time)
    {
@@ -176,11 +197,32 @@ function Result_game(result)
    if(result)
    {
       console.log("Game over!, you win!!!");
+      document.getElementById('title_result').innerText = "Ты выиграл!";
    }
    else
    {
       console.log("stop! you lose(")
+      document.getElementById('title_result').innerText = "Ты проиграл(";
+
    }
+
+   document.getElementById('title_timer').style.display= "none";
+   document.getElementById('title_bombs').style.display= "none";
+   document.getElementById('title_result').style.display= "block";
+
+}
+
+function Game_settings()
+{
+   Game_stop();
+   document.getElementById('settings').style.display = "flex";
+   document.getElementById('game').style.display = "none";
+}
+
+function Show_statistics() {
+   document.getElementById('title_timer').style.display= "block";
+   document.getElementById('title_bombs').style.display= "block";
+   document.getElementById('title_result').style.display= "none";
 }
 
 
